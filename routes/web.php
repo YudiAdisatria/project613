4e9c8b3c7493cbdb28170b6f6df61f0babb06ed6
YudiAdisatria <yudi.adisatria@gmail.com>
2022-06-09 16:58:18 +0700

    Memperbaiki untuk hosting

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,48 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
+/* 
+Route::get('/config-clear', function() {
+    Artisan::call('config:clear'); 
+    return 'Configuration cache cleared!';
+});
+
+Route::get('/config-cache', function() {
+    Artisan::call('config:cache');
+    return 'Configuration cache cleared! <br> Configuration cached successfully!';
+});
+
+Route::get('/cache-clear', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache cleared!';
+});
+
+Route::get('/view-cache', function() {
+    Artisan::call('view:cache');
+    return 'Compiled views cleared! <br> Blade templates cached successfully!';
+});
+
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    return 'Compiled views cleared!';
+});
+
+Route::get('/route-cache', function() {
+    Artisan::call('route:cache');
+    return 'Route cache cleared! <br> Routes cached successfully!';
+});
+
+Route::get('/route-clear', function() {
+    Artisan::call('route:clear');
+    return 'Route cache cleared!';
+});
+
+Route::get('/storage-link', function() {
+    Artisan::call('storage:link');
+    return 'The links have been created.';
+});
+*/
+
 Route::get('/', function () {
     return redirect('/login');
 });
